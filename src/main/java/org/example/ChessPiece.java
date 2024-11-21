9199894b625fba3706c9c855307f8c1f0f217f65
Egor Manakov <Jorjee.koff@yandex.ru>
2024-11-21 19:22:19 +0300

    2.7.2 добавлен конь добавлен метод проверки позиции в ChessPiece

--- src/main/java/org/example/ChessPiece.java
+++ src/main/java/org/example/ChessPiece.java
@@ -1,11 +1,18 @@
 package org.example;
 
 abstract public class ChessPiece {
+
+    public ChessPiece(String color){
+        this.color = color;
+    }
     String color;
     boolean check;
 
     public String getColor(){
         return color;
+    }
+    public static boolean positionInRange(int toLine, int toColumn){
+        return (toLine >= 0) && (toLine <= 7) && (toColumn >= 0) && (toColumn <= 7);
     }
     abstract public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
     abstract public String getSymbol();
---
+++ src/main/java/org/example/Horse.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import static java.lang.Math.abs;
+
+public class Horse extends ChessPiece{
+    public Horse(String color) {
+        super(color);
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        return ((abs(column - toColumn) == 2) && (abs(line - toLine) == 1))||
+                ((abs(column - toColumn) == 1) && (abs(line - toLine) == 2))&&
+                positionInRange(toLine, toColumn);
+    }
+
+    @Override
+    public String getSymbol() {
+        return "H";
+    }
+}
